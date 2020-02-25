0e6a93e2c9f97e1fed551759bc9398c9d9786ab7
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-02-25 12:22:17 +0800

    read records from a text file

---
+++ read_file.cbl
@@ -0,0 +1,38 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       *> Note that on Mainframes server, we do not use text files; instead we use PS files.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. READ-FILE.
+       ENVIRONMENT DIVISION.
+           INPUT-OUTPUT SECTION.
+               FILE-CONTROL.
+               SELECT STUDENT ASSIGN TO 'input.txt'
+               *> Note that the input.txt must be in the bin folder.
+               ORGANIZATION IS LINE SEQUENTIAL.
+       DATA DIVISION.
+           FILE SECTION.
+           FD STUDENT.
+           01 STUDENT-FILE.
+               05 STUDENT-ID PIC 9(5).
+               05 NAME PIC A(25).
+           WORKING-STORAGE SECTION.
+           01 WS-STUDENT.
+               05 WS-STUDENT-ID PIC 9(5).
+               05 WS-NAME PIC A(25).
+           01 WS-EOF PIC A(1).
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           OPEN INPUT STUDENT.
+               PERFORM UNTIL WS-EOF='Y'
+                   READ STUDENT INTO WS-STUDENT
+                       AT END MOVE 'Y' TO WS-EOF
+                       NOT AT END DISPLAY WS-STUDENT
+                   END-READ
+               END-PERFORM.
+           CLOSE STUDENT.
+       STOP RUN.
+       END PROGRAM READ-FILE.
